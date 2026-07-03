aa2cc5fa3896cfb22b600d04e44babc763808251
IsaacLCH12 <isaaclivaque123@gmail.com>
2026-07-02 20:16:27 -0500

    Actualizar dashboard-admin.ts

--- src/app/page/admin/dashboard-admin/dashboard-admin.ts
+++ src/app/page/admin/dashboard-admin/dashboard-admin.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { PacienteService } from '../../../../core/services/paciente.service';
+import { MedicoService } from '../../../../core/services/medico.service';
+import { SedeService } from '../../../../core/services/sede.service';
 
 @Component({
   selector: 'app-dashboard-admin',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './dashboard-admin.html',
-  styleUrl: './dashboard-admin.scss',
+  styleUrls: ['./dashboard-admin.scss']
 })
-export class DashboardAdmin {
+export class DashboardAdmin implements OnInit {
+  private pacienteService = inject(PacienteService);
+  private medicoService = inject(MedicoService);
+  private sedeService = inject(SedeService);
 
+  totalPacientes = 0;
+  totalMedicos = 0;
+  totalSedes = 0;
+
+  ngOnInit() {
+    // Consumimos las apis para sacar las métricas en tiempo real
+    this.pacienteService.getAllPacientes().subscribe(res => this.totalPacientes = res.length);
+    this.medicoService.getAllMedicos().subscribe(res => this.totalMedicos = res.length);
+    this.sedeService.getSedesActivas().subscribe(res => this.totalSedes = res.length);
+  }
 }
