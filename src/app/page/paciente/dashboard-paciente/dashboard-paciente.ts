710fcbfdbd582351cbebd148bc78977d1156cae8
IsaacLCH12 <isaaclivaque123@gmail.com>
2026-07-02 21:00:37 -0500

    Actualizar dashboard-paciente.ts

--- src/app/page/paciente/dashboard-paciente/dashboard-paciente.ts
+++ src/app/page/paciente/dashboard-paciente/dashboard-paciente.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { PacienteService } from '../../../../core/services/paciente.service';
+import { PacienteService } from '../../../core/services/paciente.service';
 
 @Component({
   selector: 'app-dashboard-paciente',
@@ -11,13 +11,13 @@ import { PacienteService } from '../../../../core/services/paciente.service';
 })
 export class DashboardPaciente implements OnInit {
   private pacienteService = inject(PacienteService);
-  nombrePaciente = 'Cargando...'; // Texto por defecto mientras consulta a la BD
+  nombrePaciente = 'Cargando...';
 
   ngOnInit() {
     const id = localStorage.getItem('usuarioId');
     if (id) {
-      this.pacienteService.getPaciente(id).subscribe(res => {
-        this.nombrePaciente = res.nombre; // Aquí atrapamos el nombre real
+      this.pacienteService.getPaciente(id).subscribe((res: any) => {
+        this.nombrePaciente = res.nombre;
       });
     }
   }
