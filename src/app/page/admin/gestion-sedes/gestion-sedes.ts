1b4762119950c020b416b70b56e8fe84883a519d
IsaacLCH12 <isaaclivaque123@gmail.com>
2026-07-02 21:43:08 -0500

    Actualizar gestion-sedes.ts

--- src/app/page/admin/gestion-sedes/gestion-sedes.ts
+++ src/app/page/admin/gestion-sedes/gestion-sedes.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SedeService } from '../../../../core/services/sede.service';
 
 @Component({
   selector: 'app-gestion-sedes',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './gestion-sedes.html',
-  styleUrl: './gestion-sedes.scss',
+  styleUrls: ['./gestion-sedes.scss']
 })
-export class GestionSedes {
+export class GestionSedes implements OnInit {
+  private sedeService = inject(SedeService);
+  listaSedes: any[] = [];
 
+  ngOnInit() {
+    this.sedeService.getSedesActivas().subscribe((res: any[]) => {
+      this.listaSedes = res;
+    });
+  }
 }
