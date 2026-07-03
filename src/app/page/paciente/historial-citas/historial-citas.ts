aa917d93c8f3a5e9bb490f31755752f24277d695
IsaacLCH12 <isaaclivaque123@gmail.com>
2026-07-02 20:00:27 -0500

    Actualizar historial-citas.ts

--- src/app/page/paciente/historial-citas/historial-citas.ts
+++ src/app/page/paciente/historial-citas/historial-citas.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CitaService } from '../../../../core/services/cita.service';
 
 @Component({
   selector: 'app-historial-citas',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './historial-citas.html',
-  styleUrl: './historial-citas.scss',
+  styleUrls: ['./historial-citas.scss']
 })
-export class HistorialCitas {
+export class HistorialCitas implements OnInit {
+  citasPasadas: any[] = [];
+  private citaService = inject(CitaService);
 
+  ngOnInit() {
+    this.citaService.getAllCitas().subscribe(res => {
+      this.citasPasadas = res.filter(c => c.estado === 'ATENDIDO' || c.estado === 'CANCELADO');
+    });
+  }
 }
