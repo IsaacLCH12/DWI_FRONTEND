0871f19e4d7c832ee00c68c0371e2886681fe817
IsaacLCH12 <isaaclivaque123@gmail.com>
2026-07-02 21:01:23 -0500

    Actualizar mis-citas.ts

--- src/app/page/paciente/mis-citas/mis-citas.ts
+++ src/app/page/paciente/mis-citas/mis-citas.ts
@@ -1,36 +1,57 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CitaService } from '../../../../core/services/cita.service';
+import { CitaService } from '../../../core/services/cita.service';
+import { ModalConfirmacion } from '../../../shared/components/modal-confirmacion/modal-confirmacion';
+import { SpinnerCarga } from '../../../shared/components/spinner-carga/spinner-carga';
 
 @Component({
   selector: 'app-mis-citas',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ModalConfirmacion, SpinnerCarga],
   templateUrl: './mis-citas.html',
   styleUrls: ['./mis-citas.scss']
 })
 export class MisCitas implements OnInit {
   private citaService = inject(CitaService);
   citasPendientes: any[] = [];
+  
+  cargandoDatos = true;
+  mostrarModal = false;
+  idCitaACancelar: number | null = null;
 
   ngOnInit() {
     this.cargarCitas();
   }
 
   cargarCitas() {
-    this.citaService.getAllCitas().subscribe(res => {
-      this.citasPendientes = res.filter(c => c.estado === 'PENDIENTE'); 
+    this.cargandoDatos = true;
+    this.citaService.getAllCitas().subscribe((res: any[]) => {
+      this.citasPendientes = res.filter((c: any) => c.estado === 'PENDIENTE'); 
+      this.cargandoDatos = false;
     });
   }
 
-  cancelarCita(citaId: number) {
-    if (confirm('¿Estás seguro de cancelar esta cita?')) {
-      this.citaService.cancelarCita(citaId).subscribe({
+  abrirModalCancelacion(citaId: number) {
+    this.idCitaACancelar = citaId;
+    this.mostrarModal = true;
+  }
+
+  cerrarModal() {
+    this.mostrarModal = false;
+    this.idCitaACancelar = null;
+  }
+
+  ejecutarCancelacionReal() {
+    if (this.idCitaACancelar) {
+      this.citaService.cancelarCita(this.idCitaACancelar).subscribe({
         next: () => {
-          alert('Cita cancelada exitosamente');
+          this.mostrarModal = false;
           this.cargarCitas();
         },
-        error: () => alert('Hubo un error al intentar cancelar')
+        error: () => {
+          alert('Hubo un error al intentar cancelar');
+          this.mostrarModal = false;
+        }
       });
     }
   }
