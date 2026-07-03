5c36741482241a2bbff368e82a3f52713121cf93
IsaacLCH12 <isaaclivaque123@gmail.com>
2026-07-02 20:17:58 -0500

    Actualizar gestion-medicos.ts

--- src/app/page/admin/gestion-medicos/gestion-medicos.ts
+++ src/app/page/admin/gestion-medicos/gestion-medicos.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MedicoService } from '../../../../core/services/medico.service';
 
 @Component({
   selector: 'app-gestion-medicos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './gestion-medicos.html',
-  styleUrl: './gestion-medicos.scss',
+  styleUrls: ['./gestion-medicos.scss']
 })
-export class GestionMedicos {
+export class GestionMedicos implements OnInit {
+  private medicoService = inject(MedicoService);
+  listaMedicos: any[] = [];
 
+  ngOnInit() {
+    this.medicoService.getAllMedicos().subscribe(res => {
+      this.listaMedicos = res;
+    });
+  }
 }
