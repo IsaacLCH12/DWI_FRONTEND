af04e062563a6f2df4d5d5827d18d2272e1276d3
IsaacLCH12 <isaaclivaque123@gmail.com>
2026-07-02 20:10:23 -0500

    Actualizar login.ts

--- src/app/page/auth/login/login.ts
+++ src/app/page/auth/login/login.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrls: ['./login.scss']
 })
 export class Login {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
+  loginForm: FormGroup = this.fb.group({
+    identificador: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe({
+        next: (res) => {
+          if (res.rol === 'ADMIN') {
+            this.router.navigate(['/admin']);
+          } else if (res.rol === 'PACIENTE') {
+            this.router.navigate(['/paciente']);
+          }
+        },
+        error: () => alert('Credenciales incorrectas. Verifica tu DNI/Correo y contraseña.')
+      });
+    }
+  }
 }
