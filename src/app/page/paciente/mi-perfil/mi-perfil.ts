4195bb21e890807f3b7c9ab823d51882725e1b9d
IsaacLCH12 <isaaclivaque123@gmail.com>
2026-07-02 20:01:28 -0500

    Actualizar mi-perfil.ts

--- src/app/page/paciente/mi-perfil/mi-perfil.ts
+++ src/app/page/paciente/mi-perfil/mi-perfil.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { PacienteService } from '../../../../core/services/paciente.service';
 
 @Component({
   selector: 'app-mi-perfil',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './mi-perfil.html',
-  styleUrl: './mi-perfil.scss',
+  styleUrls: ['./mi-perfil.scss']
 })
-export class MiPerfil {
+export class MiPerfil implements OnInit {
+  private pacienteService = inject(PacienteService);
+  usuarioId = localStorage.getItem('usuarioId') || '';
+  pacienteData: any = {};
 
+  ngOnInit() {
+    if(this.usuarioId) {
+      this.pacienteService.getPaciente(this.usuarioId).subscribe(res => {
+        this.pacienteData = res;
+      });
+    }
+  }
+
+  actualizarPerfil() {
+    this.pacienteService.actualizarPaciente(this.usuarioId, this.pacienteData).subscribe({
+      next: () => alert('Perfil actualizado con éxito'),
+      error: () => alert('Error al intentar actualizar los datos')
+    });
+  }
 }
