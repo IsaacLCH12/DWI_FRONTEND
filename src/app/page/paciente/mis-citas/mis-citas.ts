e64ff6a6ea76e4c5b9ef40b118519616bb3c2fea
IsaacLCH12 <isaaclivaque123@gmail.com>
2026-07-02 19:32:19 -0500

    Actualizar mis-citas.ts

--- src/app/page/paciente/mis-citas/mis-citas.ts
+++ src/app/page/paciente/mis-citas/mis-citas.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CitaService } from '../../../../core/services/cita.service';
 
 @Component({
   selector: 'app-mis-citas',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './mis-citas.html',
-  styleUrl: './mis-citas.scss',
+  styleUrls: ['./mis-citas.scss']
 })
-export class MisCitas {
+export class MisCitas implements OnInit {
+  private citaService = inject(CitaService);
+  citasPendientes: any[] = [];
 
+  ngOnInit() {
+    this.cargarCitas();
+  }
+
+  cargarCitas() {
+    this.citaService.getAllCitas().subscribe(res => {
+      this.citasPendientes = res.filter(c => c.estado === 'PENDIENTE'); 
+    });
+  }
+
+  cancelarCita(citaId: number) {
+    if (confirm('¿Estás seguro de cancelar esta cita?')) {
+      this.citaService.cancelarCita(citaId).subscribe({
+        next: () => {
+          alert('Cita cancelada exitosamente');
+          this.cargarCitas();
+        },
+        error: () => alert('Hubo un error al intentar cancelar')
+      });
+    }
+  }
 }
