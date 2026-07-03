862d0d2fc1510346c9f17061f183594931a72f6e
IsaacLCH12 <isaaclivaque123@gmail.com>
2026-07-02 20:01:04 -0500

    Actualizar mis-pagos.ts

--- src/app/page/paciente/mis-pagos/mis-pagos.ts
+++ src/app/page/paciente/mis-pagos/mis-pagos.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PagoService } from '../../../../core/services/pago.service';
 
 @Component({
   selector: 'app-mis-pagos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './mis-pagos.html',
-  styleUrl: './mis-pagos.scss',
+  styleUrls: ['./mis-pagos.scss']
 })
-export class MisPagos {
+export class MisPagos implements OnInit {
+  pagos: any[] = [];
+  private pagoService = inject(PagoService);
 
+  ngOnInit() {
+    this.pagoService.getPagos().subscribe(res => {
+      this.pagos = res;
+    });
+  }
 }
